e607070cd8cbc2c991e31085fde28607820cea69
RaynierPM <raynier.p@blockchanger.io>
2025-11-11 09:38:46 -0400

    [Feat] Replacing actual configuration in case exists

--- install.sh
+++ install.sh
@@ -8,7 +8,20 @@ CONFIGURATION_PATH="$(cd -- $(dirname $BASH_SOURCE) && pwd)"
 TARGET_PATH=$HOME/.config/nvim
 exec 2>> "$CONFIGURATION_PATH/installation.log"
 
-echo "Cloning configuration files"
+if [ -n $(ls $TARGET_PATH) ]; then
+  echo "There are files in your configuration"
+  echo "Do you want to replace your configuration with this new config (Y/n):"
+  read answer
+  answer="${answer:0:1}"
+  if [[ $answer != "y" && $answer != "Y" ]]; then
+    echo "Installation failed: User cancelled the action"
+    exit 1
+  fi
+
+  rm -r "$TARGET_PATH/*"
+fi
+
+echo "Cloning configuration files..."
 
 mkdir -p $TARGET_PATH
 ln -s $CONFIGURATION_PATH/*.lua $TARGET_PATH
